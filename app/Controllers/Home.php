9cf59725fcfcbbeb70539c3367b15ad5bfa015fa
websshinhyuk <webs.shinhyuk@gmail.com>
2022-11-01 13:59:30 +0900

    회원가입 구현 중

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -13,7 +13,34 @@ class Home extends BaseController
 
     public function register(){
 
-        $user =  new UserModel();
+        $validate = $this->validate([
+            'user_id' => [
+                'rules'=>'required',
+                'errors'=> ['required'=>'아이디를 입력해 주세요.'],
+            ],
+            'user_name' => [
+                'rules'=>'required',
+                'errors'=> ['required'=>'이름을 입력해 주세요.'],
+            ],
+            'user_password' => [
+                'rules'=>'required',
+                'errors'=> ['required'=>'비밀번호를 입력해 주세요.'],
+            ],
+            'user_phone' => [
+                'rules'=>'required',
+                'errors'=> ['required'=>'전화번호를 입력해 주세요.'],
+            ],
+            'user_addressr' => [
+                'rules'=>'required',
+                'errors'=> ['required'=>'주소를 입력해 주세요.'],
+            ],
+        ]);
+
+        if(!$validate){
+
+        }else{
+            
+        }
 
         return view('main\register');
     }

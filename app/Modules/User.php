ce034d94e4c213235f8a874aa9dfa4461109d145
websshinhyuk <webs.shinhyuk@gmail.com>
2022-11-04 10:20:56 +0900

    관리자 화면 작업 중

--- app/Modules/User.php
+++ app/Modules/User.php
@@ -6,11 +6,18 @@ class User{
     
     public function passwordCheck($info){
         $user = new UserModel();
-        $user_info = $user->where('usr_id', $info['user_id'])->get()->getRowArray();
-        if(!empty($user_info)){
-            $result = password_verify($info['user_password'], $user_info['usr_password']);
+        $userInfo = $user->where('usr_id', $info['user_id'])->get()->getRowArray();
+        if(!empty($userInfo)){
+            $result = password_verify($info['user_password'], $userInfo['usr_password']);
             if($result){
-                
+                $session = session();
+                $sessionData = [
+                    'u_idx' => $userInfo['usr_idx'],
+                    'u_id'  => $userInfo['usr_id'],
+                    'u_name'=> $userInfo['usr_name'],
+                    'u_level'=> $userInfo['usr_level'],
+                ];
+                $session->set($sessionData);
             }
             return $result;
         }else{

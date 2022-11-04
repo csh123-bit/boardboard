f696ff30f8ddd0345e32476d09803c2373c001c7
websshinhyuk <webs.shinhyuk@gmail.com>
2022-11-04 10:08:59 +0900

    관리자 화면 작업 중

--- app/Views/admin/main/login.php
+++ app/Views/admin/main/login.php
@@ -16,11 +16,11 @@
   <form method="POST">
     <div class="form-group">
       <label for="user_id">Email:</label>
-      <input type="text" class="form-control" id="user_id" placeholder="아이디" name="user_id">
+      <input type="text" class="form-control" id="user_id" placeholder="아이디" name="user_id" required>
     </div>
     <div class="form-group">
       <label for="user_password">Password:</label>
-      <input type="password" class="form-control" id="user_password" placeholder="비밀번호" name="user_password">
+      <input type="password" class="form-control" id="user_password" placeholder="비밀번호" name="user_password" required>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>

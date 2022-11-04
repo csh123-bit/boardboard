6d1f6cdcd96c999930345fa99e4adf233d45b7ab
websshinhyuk <webs.shinhyuk@gmail.com>
2022-11-04 10:14:03 +0900

    관리자 화면 작업 중

--- app/Modules/User.php
+++ app/Modules/User.php
@@ -9,6 +9,9 @@ class User{
         $user_info = $user->where('usr_id', $info['user_id'])->get()->getRowArray();
         if(!empty($user_info)){
             $result = password_verify($info['user_password'], $user_info['usr_password']);
+            if($result){
+                
+            }
             return $result;
         }else{
             return false;

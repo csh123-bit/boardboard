4ccba14e1267ec0b62174bef32b0a15597af8ad8
test <cordial14@hanmail.net>
2022-11-27 08:17:29 +0900

    데이터베이스 변경

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,7 +33,7 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'root',
+        'username' => 'postgres',
         'password' => '12qwas',
         'database' => 'boardboard',
         'DBDriver' => 'Postgre',
